5ed8f913d2cb489634fe64ef1101f3b858c777c2
Marcos Lima <mrsk1414@gmail.com>
2025-03-02 12:03:13 -0300

    Refactor: Finish the project

--- src/app.js
+++ src/app.js
@@ -10,45 +10,71 @@ const items = [
         id: 1,
         name: "Maçã",
         quantity: 1,
-        type: "Fruta",
+        type: "Fruta"
     },
     {
         id: 2,
         name: "Laranja",
         quantity: 2,
-        type: "Fruta",
+        type: "Fruta"
     }
-]
+];
+const validNames = [items[0].name, items[1].name];
+
 
 app.listen(5000, () => {
     console.log("Funcionou");
 });
 
-// pega todas os itens
 app.get("/items", (req, res) => {
-    res.send(items);
-})
+    const { type } = req.query;
 
-// pega o item pelo ID
+    if (type) {
+        const typeFilter = items.filter(item => {
+            return item.type.includes(type)
+        }
+        );
+        return res.send(typeFilter);
+    }
+
+    res.send(items);
+});
+
 app.get("/items/:id", (req, res) => {
     const id = req.params.id;
     const item = items.find(item => {
         return item.id === Number(id);
     })
-    res.send(item);
-})
 
-// recebe item 
+    if (Number(id) <= 0 || !Number.isInteger(Number(id))) {
+        res.sendStatus(400);
+        return;
+    }
+    if (Number(id) > items.length) {
+        res.sendStatus(404);
+        return;
+    }
+
+    res.send(item);
+});
+
 app.post("/items", (req, res) => {
     const item = req.body;
 
-    if(!item.name || !item.quantity || !item.type) {
+    if (!item.name || !item.quantity || !item.type) {
         res.status(422).send("Preencha corretamente os dados name, quantity e type.")
-        return
+        return;
+    }
+
+    if (validNames.includes(item.name)) {
+        res.status(409).send("O recurso que você está tentando inserir já foi inserido.")
+        return;
     }
 
     items.push({
         id: items.length + 1,
-        ...item});
-    res.send("Item recebido");
-})
+        ...item
+    });
+    validNames.push(item.name);
+    res.status(201).send("Item recebido!");
+});
